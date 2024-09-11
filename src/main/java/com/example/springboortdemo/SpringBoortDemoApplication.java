34dec75335bd9f9cf277e75b4b1c3e1150824ff8
Rob-WildBreath <mryaow@163.com>
2024-09-11 09:08:58 +0800

    Strat

---
+++ src/main/java/com/example/springboortdemo/SpringBoortDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboortdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoortDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoortDemoApplication.class, args);
+    }
+
+}
