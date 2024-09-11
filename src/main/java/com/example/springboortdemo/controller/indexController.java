2b9af5cc2f37f503299bd9d8353db1bb19c214c4
Rob-WildBreath <mryaow@163.com>
2024-09-11 09:42:40 +0800

    增加MAV /index?name=，去除原有的/index页面

--- src/main/java/com/example/springboortdemo/controller/indexController.java
+++ src/main/java/com/example/springboortdemo/controller/indexController.java
@@ -2,9 +2,24 @@ package com.example.springboortdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @Controller
 public class indexController {
+
+    //?name=
     @RequestMapping("/index")
-    public String index() {return "index";}
+    public ModelAndView index2(@RequestParam String name) {
+
+        ModelAndView modelAndView = new ModelAndView("index");
+
+        Map<String, Object> model = modelAndView.getModel();
+
+        model.put("name", name);
+
+        return modelAndView;
+    }
 }
