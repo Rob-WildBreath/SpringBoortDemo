50a65add16189ee1a21d78771aab1b61b2a0c8d6
Rob-WildBreath <mryaow@163.com>
2024-09-23 09:13:40 +0800

    增加index显示时间与ip

--- src/main/java/com/example/springboortdemo/controller/loginController.java
+++ src/main/java/com/example/springboortdemo/controller/loginController.java
@@ -1,24 +1,56 @@
 package com.example.springboortdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Controller
 public class loginController {
+
+    public String getClientIP(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+    public String getCurrentTime(){
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return now.format(formatter);
+    }
 
     @RequestMapping("/login")
     public String login() {return "login";}
 
     @PostMapping("/login")
-    public ModelAndView loginPost(@RequestParam("username")String username, @RequestParam("password")String password) {
+    public ModelAndView loginPost(@RequestParam("username")String username, @RequestParam("password")String password, HttpServletRequest request) {
         ModelAndView mav = new ModelAndView("login");
         mav.addObject("username", username);
         if(username.equals("admin") && password.equals("admin")) {
             mav.setViewName("index");
             mav.addObject("message", "You have successfully logged in!");
+            mav.addObject("IP", getClientIP(request));
+            mav.addObject("time", getCurrentTime());
         }
         else {
             mav.setViewName("login");
